1929de6fd816a98009ff807c7a8170dc858eb6c0
Amir Ali <41503327+AmirAli5@users.noreply.github.com>
2019-02-04 13:01:55 +0500

    Add files via upload

---
+++ 1.Portfolio Site/index.php
@@ -0,0 +1,269 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta http-equiv="content-type" content="text/html"; charset="utf-8">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Computer Science">
+        <meta name="author" content="Amir Ali">
+        <link rel="icon" href="assets/img/favicon.ico">
+        
+        <title>Amir Ali</title>
+        
+        <!-- Bootstrap CSS -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- FOnt Awesome Icons -->
+        <link href="assets/css/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        
+        <!-- Google Fonts... -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+        
+        <!-- ANimate CSS-->
+        <link rel="stylesheet" href="assets/css/animate.css">
+        
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="assets/css/custom.css">
+         
+        
+        <!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>-->
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <!--<![endif]-->
+        
+        
+    </head>
+    
+<body data-spy="scroll" data-target="#scrollExample">
+    
+    <!-- ==================================================== -- >
+    
+    <!-- HEADER  -->
+        
+        <header class="head" id="one">
+            
+            <!-- NAVBAR -->
+                <nav class="navbar navbar-default navbar-inverse navbar-static-top navbar-fixed-top" id="scrollExample">
+                  <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                      <h2>Machine Learning Researcher</h2>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                      
+                      
+                      <ul class="nav navbar-nav navbar-right">
+                       <li><a href="#one">Home</a></li>
+                        <li><a href="#two">Curriculum</a></li>
+                        <li><a href="#three">About Me</a></li>
+                        <li><a href="#four">Contact Me</a></li>
+                      </ul>
+                    </div><!-- /.navbar-collapse -->
+                  </div><!-- /.container-fluid -->
+                </nav>
+                
+        </header>
+        
+    <!-- ==================================================== -->
+
+    
+    <!-- HEAD  -->
+        
+            <section class="main" data-type="background" data-speed="5">
+            <div id="theTarget"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4 col-sm-offset-4 wow fadeInDown">
+                        <img src="assets/img/amir.gif" alt="amir">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-4 col-sm-offset-4 wow fadeInLeft">
+                        <h1>Amir Ali</h1>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+    <!-- ==================================================== -- >
+
+
+   
+    <!-- PORTFOLIO  -->
+        <section class="port" id="two" >
+            
+            <div class="container ">
+               
+                <div class="row">
+                    <div class="col-sm-4 col-sm-offset-4">
+                        <h1>Curriculum</h1>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-sm-5 hovereffect wow fadeInLeft">
+                        <img class="img-responsive" src="assets/img/education%20(2).jpeg" alt="site 1">
+                        <div class="overlay">
+                           <h2>Education</h2>
+                       <a class="info" href="https://www.linkedin.com/in/amir-ali-42926a156/" target="_blank" >Click!</a>
+                    </div>
+                    </div>
+                    <div class="col-sm-5 col-sm-offset-2 hovereffect wow fadeInRight">
+                        <img class="img-responsive" src="assets/img/experience.jpeg" alt="site 1">
+                        <div class="overlay">
+                           <h2>Experience</h2>
+                       <a href="https://www.linkedin.com/in/amir-ali-42926a156/" target="_blank" >Click!</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-5  hovereffect wow fadeInLeft" >
+                        <img class="img-responsive" src="assets/img/Projects.jpeg" alt="site 1">
+                        <div class="overlay">
+                           <h2>Projects</h2>
+                       <a class="info" href="https://www.linkedin.com/in/amir-ali-42926a156/" target="_blank" >Click!</a>
+                    </div>
+                    </div><div class="col-sm-5 col-sm-offset-2  hovereffect wow fadeInRight">
+                        <img class="img-responsive" src="assets/img/pexels-photo-207700.jpeg" alt="site 1">
+                        <div class="overlay">
+                           <h2>Publication</h2>
+                       <a class="info" href="https://www.linkedin.com/in/amir-ali-42926a156/" target="_blank" >Click!</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+    <!-- ==================================================== -->
+
+
+   
+   
+    <!-- ABOUT ME  -->
+        <section class="aboutMe" id="three">
+            <div class="container">
+                <div class="row">
+                   <div class="col-sm-12">
+                       <h1>About Me</h1>
+                   </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        
+                            <p> Hi! My name is Amir Ali and I am from Pakistan.I am student of Computer science in UET Lahore.I am currently part of cancer research team of the Wavy Group Part of current ongoing research projects.I am a ambitious, self-made, work alcoholic but down to earth person. I like to balance professional life.I would describe myself as someone who is honest, caring, intelligent, hardworking, and ambitious. I have a great sense of humour. I am an easy going person & don’t get easily disturbed by down’s in my life. I a spiritual person & have a good library of best spiritual books. I also enjoy travelling, Playing , going out for dinner, and having great intellectual conversations!
+                            
+                        </p>
+                    
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+        
+    <!-- ==================================================== -->
+
+   
+        
+        
+    <!-- ==================================================== -->
+    
+    <!-- contact section starts here -->
+			<br>
+			<section id="contact">
+				<div class="container">
+					<div class="row">
+							<div class="contact-caption clearfix">
+								<div class="contact-heading text-center">
+									<h2>Contact Me</h2>
+								</div>
+
+								<div class="col-md-5 contact-info text-left">
+									<h3>contact information</h3>
+									<div class="info-detail">
+										<ul><li><i class="fa fa-calendar"></i><span>24/7</span></li></ul>
+										<ul><li><i class="fa fa-map-marker"></i><span>Address:</span> NFC IEFR Jaranwala Road, Faisalabad.</li></ul>
+										<ul><li><i class="fa fa-phone"></i><span>Phone:</span>+923043595602</li></ul>
+										
+										<ul><li><i class="fa fa-envelope"></i><span>Email:</span>amirali.cheema@outlook.com</li></ul>
+									</div>
+								</div>
+
+
+								<div class="col-md-6 col-md-offset-1 contact-form">
+									<h3>Send a message</h3>
+
+									<form class="form" action="index.html" method="post">
+										<input class="name" type="text" placeholder="Name">
+										<input class="email" type="email" placeholder="Email">
+										<input class="phone" type="text" placeholder="Phone No:">
+										<textarea class="message" name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+										<input class="submit-btn" type="submit" value="Send">
+									</form>
+								</div>
+
+							</div>
+					</div>
+				</div>
+                  
+			</section>
+    <br><!-- end of contact section -->
+
+    
+    
+    
+    
+   
+   
+    <!-- FOOTER  -->
+        <footer id="foot">
+        
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <p>© All right reserved</p>
+                    </div>
+                    <div class="col-sm-5">
+                        <center><a href="https://web.facebook.com" target="_blank" ><img src="assets/img/Facebook-icon.png" height="20" width="20" ></a>
+                         <a href="https://twitter.com" target="_blank" ><img src="assets/img/twitter.png" height="20" width="20" ></a>
+                         <a href="https://web.skype.com" target="_blank" ><img src="assets/img/Skype.png" height="20" width="20" ></a>
+                            <a href="https://www.linkedin.com" target="_blank" ><img src="assets/img/linkedin-blue-style-logo-png-0.png" height="20" width="20" ></a>
+                         <a href="https://github.com" target="_blank" ><img src="assets/img/github.png" height="20" width="20" ></a></center>
+                    </div>
+                    
+                
+                    <div class="col-sm-3 col-sm-offset-1">
+                        <small><div id="clockbox"></div></small>
+                        
+                    </div>
+                </div>
+            </div>
+        
+        </footer>
+    <!-- ==================================================== -- >
+
+   
+    <!-- BOOTSTRAP-CORE JS... (placed at end so the page loads faster) -->
+    <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="assets/js/jquery-3.1.1.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+     <script>
+      new WOW().init();
+      </script>
+    
+</body>
+</html>
